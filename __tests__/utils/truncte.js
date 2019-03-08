b4abcc5dd7ff15860ac12af13f7cf0b502ad5003
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-08 16:58:50 -0300

    feat(tests): Truncate tables
    Always after the run the tests, even if its fail, is necessary to truncate all data created by tests

---
+++ __tests__/utils/truncte.js
@@ -0,0 +1,15 @@
+/**
+ * Always clean the database after tests.
+ */
+const { sequelize } = require('../../src/app/models');
+
+module.exports = () => {
+	return Promise.all(
+		Object.keys(sequelize.models).map(key => {
+			return sequelize.models[key].destroy({
+				truncate: true,
+				force: true
+			});
+		})
+	);
+};
