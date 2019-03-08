f30d77f441fda8085225b7cd4be4d5df7d527165
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-08 16:07:50 -0300

    refactor(config): Enviroment variables
    Configure the database config with .env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
+/**
+ * This code is repeted to use on tests. When the tests runs, the app context is not present.
+ */
+require('dotenv').config({
+	path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
+
 module.exports = {
-	host: '127.0.0.1',
-	username: 'postgres',
-	password: '123456',
-	database: 'node-auth-db',
-	dialect: 'postgres',
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	dialect: process.env.DB_DIALECT || 'postgres',
+	storage: './__tests__/database.sqlite', // define path to sqlite tests db
 	operatorsAliases: false, // disable sequelize warning
 	logging: false, // show minus logs when migrations runs
 	define: {
