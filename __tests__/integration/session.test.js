f13939d9c12c883e272aa27cb5cb17dfb01abf1c
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-08 16:24:01 -0300

    test(tests): Sqlite configurations
    Adding sqlite configurations for integration tests. Adding migration rules pre to create migration
    and post to clean migration in test database

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,5 +1,15 @@
+const { User } = require('../../src/app/models');
+
 describe('Authentication', () => {
-	it('should return true', () => {
-		expect(true).toBe(true);
+	it('should create an user', async () => {
+		const user = await User.create({
+			name: 'Fabricio Nogueira',
+			email: 'nogsantos@gmail.com',
+			password_hash: '123456'
+		});
+
+		console.log('user', user);
+
+		expect(user.email).toBe('nogsantos@gmail.com');
 	});
 });
