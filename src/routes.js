4305a141100898582651de70fc9f108ad54dddf5
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-08 15:26:29 -0300

    refactor(routes): Remove code
    Remove test code

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,3 @@
 const routes = require('express').Router();
-const { User } = require('./app/models');
-
-User.create({
-	name: 'Fabricio Nogueira',
-	email: 'nogsantos@gmail.com',
-	password_hash: '12345'
-});
 
 module.exports = routes;
