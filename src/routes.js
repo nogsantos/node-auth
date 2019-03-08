73e7d39d7f86cd728cda4d98f89680367fadb35e
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-08 19:38:47 -0300

    feat(route): Private route
    Creating a private route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 const routes = require('express').Router();
+const authMiddleware = require('./app/middleware/auth');
 
 const SessionController = require('./app/controllers/SessionController');
 
+/**
+ * Public routes
+ */
 routes.post('/sessions', SessionController.store);
+
+/**
+ * Private routes
+ */
+routes.use(authMiddleware);
+
+routes.get('/dashboard', (req, res) => res.status(200).send());
 
 module.exports = routes;
