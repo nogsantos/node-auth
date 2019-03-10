9c833f3dcd61d9ea1e9361eb5c105abf4914f832
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-10 15:31:32 -0300

    feat(config): Cors and bodyParser
    Setting cors and body parser responses

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ require('dotenv').config({
 });
 
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 class AppController {
 	constructor() {
@@ -13,11 +15,32 @@ class AppController {
 
 	init() {
 		this.middlewares();
+		this.enableCors();
+		this.setBodyParser();
 		this.routes();
 	}
 
 	middlewares() {
 		this.express.use(express.json());
+	}
+
+	enableCors() {
+		this.express.use(cors());
+	}
+
+	setBodyParser() {
+		this.express.use(bodyParser.urlencoded({ extended: true }));
+		if (process.env.NODE_ENV === 'DEV') {
+			this.express.use('*', (req, res, next) => {
+				const start = new Date().getTime();
+				next();
+				const end = new Date().getTime();
+				const timer = end - start;
+				console.log('Request start', `${start}ms`);
+				console.log('Request end', `${end}ms`);
+				console.log('Request Timer', `${timer}ms`);
+			});
+		}
 	}
 
 	routes() {
